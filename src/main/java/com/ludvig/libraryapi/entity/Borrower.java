64d61304896f0c9961eaa99db9af6c6ca1af9a7c
Ludvig Fendert <l.fendert@gmail.com>
2026-03-17 12:56:40 +0100

    added first draft Borrower

---
+++ src/main/java/com/ludvig/libraryapi/entity/Borrower.java
@@ -0,0 +1,28 @@
+package com.ludvig.libraryapi.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "borrowers")
+public class Borrower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "borrower")
+    private List<Loan> loans;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public List<Loan> getLoans() { return loans; }
+    public void setLoans(List<Loan> loans) { this.loans = loans; }
+}
