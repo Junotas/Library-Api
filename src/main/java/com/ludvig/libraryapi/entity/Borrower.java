51c8422626280d12741d9d3023579f2d08172f1a
Ludvig Fendert <l.fendert@gmail.com>
2026-03-18 10:40:02 +0100

    refactored google-java-format for entity classes

--- src/main/java/com/ludvig/libraryapi/entity/Borrower.java
+++ src/main/java/com/ludvig/libraryapi/entity/Borrower.java
@@ -7,22 +7,45 @@ import java.util.List;
 @Table(name = "borrowers")
 public class Borrower {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String name;
-    private String email;
+  private String name;
+  private String email;
 
-    @OneToMany(mappedBy = "borrower")
-    private List<Loan> loans;
+  @OneToMany(mappedBy = "borrower")
+  private List<Loan> loans;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public List<Loan> getLoans() { return loans; }
-    public void setLoans(List<Loan> loans) { this.loans = loans; }
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public List<Loan> getLoans() {
+    return loans;
+  }
+
+  public void setLoans(List<Loan> loans) {
+    this.loans = loans;
+  }
 }
--- src/main/java/com/ludvig/libraryapi/entity/Loan.java
+++ src/main/java/com/ludvig/libraryapi/entity/Loan.java
@@ -7,26 +7,49 @@ import java.time.LocalDate;
 @Table(name = "loans")
 public class Loan {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private LocalDate loanDate;
+  private LocalDate loanDate;
 
-    @ManyToOne
-    @JoinColumn(name = "book_id")
-    private Book book;
+  @ManyToOne
+  @JoinColumn(name = "book_id")
+  private Book book;
 
-    @ManyToOne
-    @JoinColumn(name = "borrower_id")
-    private Borrower borrower;
+  @ManyToOne
+  @JoinColumn(name = "borrower_id")
+  private Borrower borrower;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public LocalDate getLoanDate() { return loanDate; }
-    public void setLoanDate(LocalDate loanDate) { this.loanDate = loanDate; }
-    public Book getBook() { return book; }
-    public void setBook(Book book) { this.book = book; }
-    public Borrower getBorrower() { return borrower; }
-    public void setBorrower(Borrower borrower) { this.borrower = borrower; }
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDate getLoanDate() {
+    return loanDate;
+  }
+
+  public void setLoanDate(LocalDate loanDate) {
+    this.loanDate = loanDate;
+  }
+
+  public Book getBook() {
+    return book;
+  }
+
+  public void setBook(Book book) {
+    this.book = book;
+  }
+
+  public Borrower getBorrower() {
+    return borrower;
+  }
+
+  public void setBorrower(Borrower borrower) {
+    this.borrower = borrower;
+  }
 }
